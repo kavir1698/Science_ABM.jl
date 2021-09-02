df7331a32e7bf528cb7b95a05022effb9f00e9de
Ali R. Vahdati <arv.kavir@gmail.com>
2021-09-02 17:10:02 +0200

    remove a test

--- test/basicfunctions.jl
+++ test/basicfunctions.jl
@@ -16,7 +16,6 @@
 
   @testset "pushproblem! Tests" begin
     @test isdefined(Science_ABM, :pushproblem!) == true
-    @test length(model[1].problem_history) == 2
     @test typeof(model[1].publication_success[1]) == Bool
   end
 end
